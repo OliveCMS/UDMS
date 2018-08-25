1d4a9ae38fbf7e13422bfe5e0319f432f204c3b0
xtoolkit <arshen@outlook.com>
2018-08-25 15:22:00 +0430

    add limit mode

--- src/Model/Table.php
+++ src/Model/Table.php
@@ -284,7 +284,7 @@ class table
             }
             $return = call_user_func_array([$this, 'orderBy'], $args);
         }
-        if (isset($options['limit']) and is_int($options['limit']) and $options['limit']>0) {
+        if (isset($options['limit']) and is_int($options['limit']) and $options['limit'] > 0) {
             $return = array_slice($return, 0, $options['limit']);
         }
 
